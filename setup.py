c4216f4770932859936b9ae46150f1e7f18cb27a
Idrisvohra9 <idrishaider987@gmail.com>
2023-06-08 13:14:41 +0530

    Name Changes due to pypi Error

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     description = fh.read()
 
 setuptools.setup(
-    name="colors",
+    name="trmnl-colors",
     version="4.0.4",
-    author="Idris Vohra",
+    author="Idris-Vohra",
     author_email="idrishaider987@gmail.com",
-    packages=["colors"],
+    packages=["trmnl-colors"],
     description="Colors, text formats, decorations and much more for TERMINAL",
     long_description=description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gituser/test-tackage",
+    url="https://github.com/Idrisvohra9/colors",
     license='MIT',
     python_requires='>=3.8',
     install_requires=[]
