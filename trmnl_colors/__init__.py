a841b2243a69003140ca23c92d2eb6e5b20922df
Idrisvohra9 <idrishaider987@gmail.com>
2023-06-08 21:52:34 +0530

    Lets hope for the best

--- trmnl_colors/__init__.py
+++ trmnl_colors/__init__.py
@@ -1,26 +1,39 @@
 """
 
-# Colors
+# trmnl_colors v-4.0.5
 ## Add colors to the static WOrLd!
 
 
 colors module is made to colorize the output of terminal when the code runs on command line, With that there are functions for adding basic text colors (Total 10 including invsisble) and or text formats such as Bold, Italics, Underline, Light or Strikethrough to your code.
 
-But wait there is more! use background colors, filled Variants which can be accessed by calling thier specific name and 'bg' postfix as in background for example:
-colors.cyanbg()
+But wait there is more! use background colors, filled Variants which can be accessed by calling thier specific name and 'bg' postfix as in background. Here is an example describing with  use case:
+```python
+from trmnl_colors import *
+bluebg()
+print("Hello")
+reset()
+```
 
 * Each color can be accessed by calling thier particular name and the first letter of the format at the postfix.
-
+```python
+from trmnl_colors import *
+yellowB()
+print("Hello") # After every color call remember to call the reset function.
+# Outputs Hello in yellow color in Bold text
+reset()
+```
 * For example:Call clr.redB() for red color with text format Bold, S for strikethrough D for Default U for Underline I for Italics L is for special lighter color substitute for each color.
 
 
 * There are also filled color variants in which the font color and the background color match each other making them viable for specific type of color operations. 
 
 * There is a special function called reset() which is basically the default white so that the color function gets reset it is recommended to use this function after every usage of a color to mantain the color of the terminal.
+* 
 * PS if you want to create blocks of colors for decoration purposes use filled variants e.g. clr.whitefilled() 
 
-Explore & stay Creative 😉
-### by> Idris vohra
+Explore & stay Creative
+### by: Idris-Vohra :O
+
 """
 def blackD(): # Default
     print("\033[0;30m",end="")
@@ -198,32 +211,32 @@ def whiteS():
     print("\033[0;8m",end="")
 
 
-# Filled variants:
-def redfilled():
+# fill variants:
+def redfill():
     print("\033[31;41m",end='')
 
-def yellowfilled():
+def yellowfill():
     print("\033[33;43m",end='')
 
-def greenfilled():
+def greenfill():
     print("\033[32;42m",end='')
 
-def bluefilled():
+def bluefill():
     print("\033[34;44m",end='')
 
-def magentafilled():
+def magentafill():
     print("\033[35;45m",end='')
 
-def cyanfilled():
+def cyanfill():
     print("\033[36;46m",end='')
 
-def whitefilled():
+def whitefill():
     print("\033[37;47m",end='')
 
-def blackfilled():
+def blackfill():
     print("\033[30;40m",end='')
 
-def greyfilled():
+def greyfill():
     print("\033[90;100m",end='')
 
 # Background:
